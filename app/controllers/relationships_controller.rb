a9a72bfe6ffa837d8d638ced5f3a49322760f498
Kevin Malta <kevin.malta@gmail.com>
2015-02-09 23:20:18 -0800

    stashing changes to go to rubymines ide

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
 	def destroy
 		group = Relationship.find(params[:id]).group_id
 	    current_user.remove_relationship(group)
-	    redirect_to chat_path    
+	    redirect_to chat_path
 	end
-	
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,14 +8,14 @@ module SessionsHelper
     	session[:user_id] = user.id
   	end
 
-  	def log_out
+	def log_out
 		session.delete(:user_id)
 		@current_user = nil
 	end
 
 	def current_user?(user)
-    	user == current_user
+  	user == current_user
 	end
 
-	
+
 end
