d88371694d598605fbd078c59958438cfc920167
Kevin Malta <kevin.malta@gmail.com>
2015-02-09 23:28:38 -0800

    forgot these changes to make chat controller

---
+++ app/controllers/chat_controller.rb
@@ -0,0 +1,17 @@
+class ChatController < ApplicationController
+
+# 	include ChatHelper
+
+#   def create
+#     group = Group.from_omniauth(env["omniauth.auth"])
+#     log_in user
+#     redirect_to '/room/:id'
+#   end
+
+#   def destroy
+#     log_out if logged_in?
+#     redirect_to root_url
+#   end
+# end
+
+end
---
+++ app/helpers/chat_helper.rb
@@ -0,0 +1,18 @@
+module ChatHelper
+
+	# def in_chat?
+	# 	!current_group.nil?
+	# end
+
+	# def end_chat
+	# 	chat.delete(:group_id)
+	# 	@current_group.chat_history = nil
+	# 	@current_group.save!
+	# 	@current_group = nil
+	# end
+
+	# def current_group?(group)
+	# 	group == current_group
+	# end
+
+end
